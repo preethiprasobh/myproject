209e08d62bf33ea61bc13c9565cf549d53fa1f5a
PreethiArun <160821896+preethiprasobh@users.noreply.github.com>
2024-03-12 14:51:53 +0530

    hello

---
+++ oopsfeatures/src/main/java/abstraction/FullTimeEmployee.java
@@ -0,0 +1,18 @@
+package abstraction;
+
+public class FullTimeEmployee extends Employee {
+
+	public FullTimeEmployee(String name, int paymentPerHour) {
+		super(name, paymentPerHour);
+		
+	}
+
+	@Override
+	public int calculateSalary() {
+		
+		return paymentPerHour*8;
+	}
+
+	
+
+}
